7c9866a9a4a5ef700e3893cd043844525c15657e
xaur <24484727+xaur@users.noreply.github.com>
2020-12-23 16:01:16 +0000

    fix space issues
    - trim trailing space
    - add space around operators
    - add missing newlines at end of files

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from functions import *
 from log import *
 from conf import *
 
-wait_time=int(config['programconfig']['checktimemins'])*60
+wait_time = int(config['programconfig']['checktimemins']) * 60
 
 while True:
     reddit_mod_log()
