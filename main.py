7eaeff8383a028f2a123575488c4501e47f3b1c8
xaur <24484727+xaur@users.noreply.github.com>
2020-12-28 15:04:30 +0000

    rename functions.py to reddit.py
    After the removal of Matrix stuff, functions.py is almost entirely
    about Reddit, so rename it to reddit.py.

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import time
 
 from conf import config
-from functions import reddit_mod_log
 import matrix
+import reddit
 
 
 wait_time = int(config["programconfig"]["checktimemins"]) * 60
 
 while True:
-    records = reddit_mod_log()
+    records = reddit.reddit_mod_log()
     for r in records:
         matrix.send_message(r)
     time.sleep(wait_time)
