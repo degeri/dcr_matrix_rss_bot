cc3bcd457eeeaf9bdbee443467054afd7ea05a5e
xaur <24484727+xaur@users.noreply.github.com>
2021-01-07 02:36:09 +0000

    comply with Matrix Client-Server API r0.6.0
    I don't know why sending worked with POST method but the spec says we
    should use PUT. Once I changed to use PUT, it stopped working until I
    added the transaction ID path component that is Required by the spec.
    
    Tested on rooms v5 and v6.
    
    https://matrix.org/docs/spec/client_server/r0.6.0#put-matrix-client-r0-rooms-roomid-send-eventtype-txnid
    
    While here, also return the response object to ease debugging.

--- matrix.py
+++ matrix.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 import requests
 
@@ -9,6 +10,9 @@ from log import logger
 CONFIG = conf.config["matrixconfig"]
 
 
+# SPEC: https://matrix.org/docs/spec/client_server/r0.6.0
+
+
 def message(body):
     return json.dumps({"msgtype": "m.text", "body": body})
 
@@ -17,9 +21,11 @@ def send_message(msg):
     token = CONFIG["accesstoken"]
     roomid = CONFIG["roomid"]
     server_url = CONFIG["server_url"]
-    url = "{}_matrix/client/r0/rooms/{}/send/m.room.message".format(
-            server_url, roomid)
+    txid = "m" + str(int(time.time()))
+    url = "{}_matrix/client/r0/rooms/{}/send/m.room.message/{}".format(
+            server_url, roomid, txid)
     headers = {"Authorization": "Bearer " + token}
     data = message(msg)
     logger.info("sending: " + msg)
-    r = requests.post(url, data=data, headers=headers)
+    r = requests.put(url, data=data, headers=headers)
+    return r
