0c6630b5b3b76642dc44054b850a33073b06c9b5
xaur <24484727+xaur@users.noreply.github.com>
2021-01-07 02:17:51 +0000

    send Matrix API token in Authorization header
    It is more secure because it has a lower chance to leak in HTTP logs.
    
    It is also recommended by the Matrix spec over passing the token in the
    query string:
    
    https://matrix.org/docs/spec/client_server/r0.6.0#using-access-tokens

--- matrix.py
+++ matrix.py
@@ -17,8 +17,9 @@ def send_message(msg):
     token = CONFIG["accesstoken"]
     roomid = CONFIG["roomid"]
     server_url = CONFIG["server_url"]
-    url = ("{}_matrix/client/r0/rooms/{}/send/m.room.message"
-           "?access_token={}").format(server_url, roomid, token)
+    url = "{}_matrix/client/r0/rooms/{}/send/m.room.message".format(
+            server_url, roomid)
+    headers = {"Authorization": "Bearer " + token}
     data = message(msg)
     logger.info("sending: " + msg)
-    r = requests.post(url, data=data)
+    r = requests.post(url, data=data, headers=headers)
