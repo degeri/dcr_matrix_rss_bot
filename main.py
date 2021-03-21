0e0afb629135f76bc7f716e36e37f4f413b43e7d
xaur <24484727+xaur@users.noreply.github.com>
2021-03-21 16:40:53 +0000

    make main module more testable

--- main.py
+++ main.py
@@ -7,10 +7,20 @@ import reddit
 
 wait_time = int(config["programconfig"]["checktimemins"]) * 60
 
-while True:
+
+def process():
     mod_actions = reddit.new_mod_actions()
     for ma in mod_actions:
         md = reddit.format_mod_action_md(ma)
         html = reddit.format_mod_action_html(ma)
         matrix.send_message(md, html)
-    time.sleep(wait_time)
+
+
+def main():
+    while True:
+        process()
+        time.sleep(wait_time)
+
+
+if __name__ == "__main__":
+    main()
